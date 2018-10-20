6d8163fdd818b05aa7d83f2599a5098cb4d6b8b9
Nils Brenneis <nilsbrenneis@arcor.de>
2018-10-20 18:30:46 +0200

    new measurement and test data implemented

--- src/main/main.go
+++ src/main/main.go
@@ -5,23 +5,9 @@ import (
 	"time"
 )
 
-type durationResults struct {
-	bestCase  []time.Duration
-	avgCase   []time.Duration
-	worstCase []time.Duration
-}
-
-var worstCaseString = "worst case:  "
-var avgCaseString = "average case:"
-var bestCaseString = "best case:   "
-
-var binSearchString = "binary search |"
-var isPrimeString = "is prime      |"
-var bubbleSortString = "bubble sort   |"
-
 var amountMeasurements = 100
 
-func binSearchAnalysis(testData []int, key int) []time.Duration {
+func measureBinSearchRun(testData []int, key int) time.Duration {
 	durations := make([]time.Duration, amountMeasurements)
 
 	for i := 0; i < len(durations); i++ {
@@ -29,74 +15,70 @@ func binSearchAnalysis(testData []int, key int) []time.Duration {
 		binarySearch(testData, key)
 		durations[i] = time.Since(start)
 	}
-	return durations
+	return genAverage(durations)
 }
 
-func isPrimeAnalysis(p int) []time.Duration {
+func measureIsPrimeRun(p int) time.Duration {
 	durations := make([]time.Duration, amountMeasurements)
-
 	for i := 0; i < len(durations); i++ {
 		start := time.Now()
 		isPrime(p)
 		durations[i] = time.Since(start)
 	}
-	return durations
+	return genAverage(durations)
 }
 
-func bubbleSortAnalysis(testData []int) []time.Duration {
+func measureBubbleSortRun(testData []int) time.Duration {
 	durations := make([]time.Duration, amountMeasurements)
-
 	for i := 0; i < len(durations); i++ {
 		start := time.Now()
 		bubbleSort(testData)
 		durations[i] = time.Since(start)
 	}
-	return durations
+	return genAverage(durations)
 }
 
-func measure() (durationResults, durationResults, durationResults) {
-	genDataBinSearch()
-	genDataIsPrime()
-	genDataBubbleSort()
-
-	bestResBinSearch := binSearchAnalysis(binSearchTestData.arr, binSearchTestData.bestCaseKey)
-	avgResBinSearch := binSearchAnalysis(binSearchTestData.arr, binSearchTestData.avgCaseKey)
-	worstResBinsearch := binSearchAnalysis(binSearchTestData.arr, binSearchTestData.worstCaseKey)
-	resultsBinSearch := durationResults{bestResBinSearch, avgResBinSearch, worstResBinsearch}
-
-	bestResIsPrime := isPrimeAnalysis(isPrimeTestData.bestCase)
-	avgResIsPrime := isPrimeAnalysis(isPrimeTestData.avgCase)
-	worstResIsPrime := isPrimeAnalysis(isPrimeTestData.worstCase)
-	resultsIsPrime := durationResults{bestResIsPrime, avgResIsPrime, worstResIsPrime}
-
-	bestResBubbleSort := bubbleSortAnalysis(bubbleSortTestData.bestCase)
-	avgResBubbleSort := bubbleSortAnalysis(bubbleSortTestData.avgCase)
-	worstResBubbleSort := bubbleSortAnalysis(bubbleSortTestData.worstCase)
-	resultsBubbleSort := durationResults{bestResBubbleSort, avgResBubbleSort, worstResBubbleSort}
-
-	return resultsBinSearch, resultsIsPrime, resultsBubbleSort
-}
-
-func genAverage(durations []time.Duration) (average time.Duration) {
-	for i := 0; i < len(durations); i++ {
-		average += durations[i]
+func measureBubbleSort(testData []int) []time.Duration {
+	avgDurations := make([]time.Duration, len(testData))
+	for i := 0; i < len(testData); i++ {
+		avgDurations[i] = measureBubbleSortRun(testData[:i])
 	}
-	tempAvg := float64(average) / float64(len(durations))
-	return time.Duration(tempAvg)
+	return avgDurations
+}
+
+func genAverage(durations []time.Duration) time.Duration {
+	var accu time.Duration
+	for i := 0; i < len(durations); i++ {
+		accu += durations[i]
+	}
+	return time.Duration(float64(accu) / float64(len(durations)))
 }
 
 func main() {
-	binSearchDurations, isPrimeDurations, bubbleSortDurations := measure()
+	bubbleSortTestData := genDataBubbleSort()
 
-	fmt.Println(binSearchString, bestCaseString, genAverage(binSearchDurations.bestCase))
-	fmt.Println(binSearchString, avgCaseString, genAverage(binSearchDurations.avgCase))
-	fmt.Println(binSearchString, worstCaseString, genAverage(binSearchDurations.worstCase))
+	durationsBestCaseBubbleSort := measureBubbleSort(bubbleSortTestData.bestCase)
+	durationsAvgCaseubbleSort := measureBubbleSort(bubbleSortTestData.avgCase)
+	durationsWorstCaseubbleSort := measureBubbleSort(bubbleSortTestData.worstCase)
+	fmt.Println(durationsBestCaseBubbleSort)
+	fmt.Println(durationsAvgCaseubbleSort)
+	fmt.Println(durationsWorstCaseubbleSort)
 
-	fmt.Println(isPrimeString, bestCaseString, genAverage(isPrimeDurations.bestCase))
-	fmt.Println(isPrimeString, avgCaseString, genAverage(isPrimeDurations.avgCase))
-	fmt.Println(isPrimeString, worstCaseString, genAverage(isPrimeDurations.worstCase))
+	isPrimeTestData := genDataIsPrime()
 
-	fmt.Println(bubbleSortString, bestCaseString, genAverage(bubbleSortDurations.bestCase))
-	fmt.Println(bubbleSortString, avgCaseString, genAverage(bubbleSortDurations.avgCase))
-	fmt.Println(bubbleSortString, worstCaseString, genAverage(bubbleSortDurations.worstCase))
+	durationsBestCaseIsPrime := measureIsPrimeRun(isPrimeTestData.bestCase)
+	durationsAvgCaseIsPrime := measureIsPrimeRun(isPrimeTestData.avgCase)
+	durationsWorstCaseIsPrime := measureIsPrimeRun(isPrimeTestData.worstCase)
+	fmt.Println(durationsBestCaseIsPrime)
+	fmt.Println(durationsAvgCaseIsPrime)
+	fmt.Println(durationsWorstCaseIsPrime)
+
+	binSearchTestData := genDataBinSearch()
+
+	durationsBestCaseBinSearch := measureBinSearchRun(binSearchTestData.arr, binSearchTestData.bestCaseKey)
+	durationsAvgCaseBinSearch := measureBinSearchRun(binSearchTestData.arr, binSearchTestData.avgCaseKey)
+	durationsWorstCaseBinSearch := measureBinSearchRun(binSearchTestData.arr, binSearchTestData.worstCaseKey)
+	fmt.Println(durationsBestCaseBinSearch)
+	fmt.Println(durationsAvgCaseBinSearch)
+	fmt.Println(durationsWorstCaseBinSearch)
 }
--- src/main/testdata.go
+++ src/main/testdata.go
@@ -27,36 +27,45 @@ var binSearchTestData binSearchStruct
 
 var isPrimeTestData isPrimeStruct
 
-var bubbleSortTestData bubbleSortStruct
+func genDataBinSearch() binSearchStruct {
+	arrLen := 1000
+	binSearchTestData.arr = make([]int, arrLen)
 
-func genDataBinSearch() {
-	binSearchTestData.arr = []int{1, 2, 89, 100, 180, 181, 1000, 1045, 1056, 1100, 3000, 3001, 3453, 3679, 4000}
-	binSearchTestData.bestCaseKey = 1045
-	binSearchTestData.avgCaseKey = 100
-	binSearchTestData.worstCaseKey = 1
+	for i := 0; i < len(binSearchTestData.arr); i++ {
+		binSearchTestData.arr[i] = i
+	}
+
+	binSearchTestData.bestCaseKey = 500
+	binSearchTestData.avgCaseKey = 999
+	binSearchTestData.worstCaseKey = 250
+	return binSearchTestData
 }
 
-func genDataIsPrime() {
+func genDataIsPrime() isPrimeStruct {
 	isPrimeTestData.bestCase = 1
 	isPrimeTestData.avgCase = 65539
 	isPrimeTestData.worstCase = 213849
+	return isPrimeTestData
 }
 
-func genDataBubbleSort() {
+func genDataBubbleSort() bubbleSortStruct {
 	arrLen := 1000
+	maxRandInt := 2000
+	var bubbleSortTestData bubbleSortStruct
 	bubbleSortTestData.bestCase = make([]int, arrLen)
+	bubbleSortTestData.avgCase = make([]int, arrLen)
+	bubbleSortTestData.worstCase = make([]int, arrLen)
+
 	for i := 1; i <= len(bubbleSortTestData.bestCase); i++ {
 		bubbleSortTestData.bestCase[i-1] = i
 	}
 
-	maxRandInt := 2000
-	bubbleSortTestData.avgCase = make([]int, arrLen)
 	for i := 0; i < len(bubbleSortTestData.avgCase); i++ {
 		bubbleSortTestData.avgCase[i] = rand.Intn(maxRandInt)
 	}
 
-	bubbleSortTestData.worstCase = make([]int, arrLen)
 	for i := 0; i < len(bubbleSortTestData.worstCase); i++ {
 		bubbleSortTestData.worstCase[i] = len(bubbleSortTestData.worstCase) - i
 	}
+	return bubbleSortTestData
 }
